5dfd45d81f3201e2c5a5132d8e687c852084fb8a
İrem Şahinli <160491598+iremSahinli@users.noreply.github.com>
2024-10-31 23:12:14 +0300

    Uı katmanı ve business katmanı oluşturuldu gerekli paketler yüklendi ve repositories yaısı eklendi.

---
+++ BordroOtomasyon.Infrastructure/Repositories/TediyeRepositories/TediyeRepository.cs
@@ -0,0 +1,12 @@
+﻿using BordroOtomasyon.Domain.Entites;
+using BordroOtomasyon.Infrastructure.Context;
+using BordroOtomasyon.Infrastructure.DataAccess.EntityFramework;
+
+
+namespace BordroOtomasyon.Infrastructure.Repositories.TediyeRepositories
+{
+    public class TediyeRepository : EFBaseRepository<Tediye>, ITediyeRepository
+    {
+        public TediyeRepository(AppDbContext context) : base(context) { }
+    }
+}
--- BordroOtomasyon.UI/Program.cs
+++ BordroOtomasyon.UI/Program.cs
@@ -1,17 +1,42 @@
+using BordroOtomasyon.Infrastructure.Extentions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace BordroOtomasyon.UI
 {
     internal static class Program
     {
+        private static ServiceProvider serviceProvider; //kuruldu.
+
+
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
+            var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.Json")
+            .Build();
+
+
+            var serviceColection = new ServiceCollection();
+            ConfigureServices(serviceColection, configuration);
+
+            serviceProvider = serviceColection.BuildServiceProvider();
+
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(serviceProvider.GetRequiredService<Form1>());
+        }
+
+        private static void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddTransient<Form1>();
+
+            //Extenstions Services Method:
+            services.AddInfrastructureServices(configuration);
+
         }
     }
 }
